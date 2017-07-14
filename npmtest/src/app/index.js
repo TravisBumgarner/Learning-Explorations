31e12fdbd198c929dafbb10ae8b4c9f420b062af
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-07-14 11:52:02 -0400

    Learning a bit about front end tech

---
+++ npmtest/src/app/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+	// render() returns what needs to be rendered
+	// jsx renders JavaScript that then writes the html out
+	render(){ 
+		return (
+			<div className = "container">
+				<div className = "row">
+					<div className = "col-xs-10 col-xs-offset-1">
+						<h1>Hello</h1>
+					</div>
+				</div>
+			</div>
+		);
+	} 
+}
+
+render(<App/>, window.document.getElementById("app")); // How it should be rendered
