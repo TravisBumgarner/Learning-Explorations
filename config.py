c6d39abb9cf380faa8b8d51173c77f03cd847fb9
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-26 19:41:59 -0400

    Progress setting up basic stuff and chat app

---
+++ config.py
@@ -0,0 +1,16 @@
+# '': {
+#     'essid': '',
+#     'password': ''
+# }
+
+ACTIVE_NETWORK = 'home'
+
+NETWORKS = {
+    'home': {
+        'essid': 'DifferentBadInternet',
+        'password': 'pizzaring280'
+    },
+}
+
+ACTIVE_NETWORK_ESSID = NETWORKS[ACTIVE_NETWORK]['essid']
+ACTIVE_NETWORK_PASSWORD = NETWORKS[ACTIVE_NETWORK]['password']
