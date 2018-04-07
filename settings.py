92e7fbd2fa59a63c8c6544daa894e90daa93d53d
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-04-07 19:47:57 -0400

    Urls are working

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Directory where html files for the project live.
+HTML_ROOT_DIR = './resources/html'
+
+# Mapping between endpoints requested and filenames. Data is fed to url.py
+URL_TO_RESOURCE = {
+    '/': 'index.html'
+}
+
+HTML_404 = '404.html'
--- url.py
+++ url.py
@@ -1,19 +1,15 @@
 import os
 
-entry_point = './src'
-
-URLS = {
-    '/': 'index.html'
-}
+from settings import URL_TO_RESOURCE, HTML_ROOT_DIR, HTML_404
 
 
 def get_body_content(request):
-    if request.path in URLS:
-        file_name = URLS[request.path]
+    if request.path in URL_TO_RESOURCE:
+        file_name = URL_TO_RESOURCE[request.path]
     else:
-        file_name = '404.html'
+        file_name = HTML_404
 
-    file_path = os.path.join(entry_point, file_name)
+    file_path = os.path.join(HTML_ROOT_DIR, file_name)
 
     with open(file_path) as f:
         body = f.read()
