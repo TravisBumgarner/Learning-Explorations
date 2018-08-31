a42b2dcd2f8485371f68410f7cc776c7ce6b872b
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-08-31 14:58:20 -0400

    Added timeit decorator

--- c1_algorithmic_toolbox/week1_programming_challenges/maximum_pairwise_product.py
+++ c1_algorithmic_toolbox/week1_programming_challenges/maximum_pairwise_product.py
@@ -1,7 +1,11 @@
 #python3
 
-# Takeaways - 
+import time
+import random
 
+from .utilities.timeit import timeit
+
+@timeit
 def mpp(length, user_inputs):
     length = int(length)
     inputs = [int(x) for x in user_inputs.split(" ")]
@@ -12,7 +16,6 @@ def mpp(length, user_inputs):
     max_vals = [float("-inf"), float("-inf")]
 
     for number in inputs:
-        print(max_vals)
         if number > max_vals[0]:
             max_vals.insert(0, number)
             max_vals.pop()
@@ -23,8 +26,29 @@ def mpp(length, user_inputs):
         
     return max_vals[0] * max_vals[1]
 
-    
+@timeit
+def mpp_sorted(length, user_inputs):
+    length = int(length)
+    inputs = [int(x) for x in user_inputs.split(" ")]
+
+    if length < 2:
+        raise(IndexError)
+
+    sorted_inputs = sorted(inputs, reverse=True)
+
+    return sorted_inputs[0] * sorted_inputs[1]
+
+
 if __name__ == "__main__":
-    length = input()
-    numbers = input()
-    print(mpp(length, numbers))
+    # length = input()
+    # numbers = input()
+    for i in range(1,10):
+        test_count = 10**i
+        numbers = str(sorted([int(random.random() * test_count) for i in range(0, test_count)], reverse=True)).replace(',','').replace('[', '').replace(']', '')
+        length = len(numbers)
+
+        print('test_count is {} - reverse sorted'.format(test_count))        
+
+        mpp(length, numbers)
+
+        mpp_sorted(length, numbers)
