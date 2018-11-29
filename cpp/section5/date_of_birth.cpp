35951159fafaa42c2d4d58fc457fc269b8e694e3
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-11-29 12:31:45 -0500

    Finished section 5

---
+++ cpp/section5/date_of_birth.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void date_of_birth() {
+    int m;
+    int d;
+    int y;
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //---- WRITE YOUR CODE BELOW THIS LINE----
+    
+    cin >> m >> d >> y;
+    
+    
+    //---- WRITE YOUR CODE ABOVE THIS LINE----
+    //---- Do NOT MODIFY THE CODE BELOW THIS LINE----
+    cout << m << " " << d << " " << y;
+}
+
+int main(){
+    date_of_birth();
+    return 0;
+}
---
+++ cpp/section5/sallys_dogs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sallys_dogs() {
+    
+    int x {3};
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE---- 
+    
+    cout << "Sally has " << x << " dogs.";
+    
+    //---- WRITE YOUR CODE ABOVE THIS LINE----
+}
