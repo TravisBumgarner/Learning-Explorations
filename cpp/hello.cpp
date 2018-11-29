54d0b583b1e40e5f8e20ea920cc7f95c2e8cf597
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-11-29 12:10:28 -0500

    Initial c++ commit

---
+++ cpp/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int num1;
+   cout << "Enter an integer: ";
+   cin >> num1;
+   cout << "You entered: " << num1;
+   return 0; 
+}
---
+++ cpp/hello_frank.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void say_hi() {
+    cout << "Hello Frank" << endl;
+}
+
+int main() {
+    say_hi();
+}
