d2fd37adcabaed0ac582c91c7499bbd69d093e9d
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-12-06 13:09:23 -0500

    What a mess I have made

--- aoc_2018/Day 6: ??/part1.py
+++ aoc_2018/Day 6: ??/part1.py
@@ -1,9 +1,17 @@
+# THIS
+# CODE
+# IS
+# BAD
+# :shrug:
+
 import re
 
 
 def process_regex(input):
-    pattern = r"(\d),\s(\d)"
+    print(input)
+    pattern = r"(\d+),\s(\d+)"
     matches = re.match(pattern, input)
+    # print(matches.groups())
     return matches.groups()
 
 
@@ -20,9 +28,9 @@ def main():
     grid_width = 0
     grid_height = 0
 
-    SHIFT_ALL_THE_THE_THINGS_BY = 20
 
     input = process_file(filename='./input.txt', single_line=False)
+    non_infinite_letters = []
     coordinates = []
     # print(input)
     for elem in input:
@@ -32,30 +40,32 @@ def main():
         y = int(y)
         coordinates.append((x, y))
         if x > grid_width:
-            grid_width = x
+            grid_width = x + 2
         if y > grid_height:
-            grid_height = y
+            grid_height = y + 2
+        
     print(grid_width, grid_height)
     grid = [[{'value': ' ', 'is_occupied': False, 'is_new': False}
-             for y in range(grid_height + 1 + SHIFT_ALL_THE_THE_THINGS_BY)] for x in range(grid_width + 1 + SHIFT_ALL_THE_THE_THINGS_BY)]
-    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             for y in range(grid_height + 1 )] for x in range(grid_width + 1 )]
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     id = 0
-    print(len(grid), len(grid[0]))
+    # print(len(grid), len(grid[0]))
     for coord in coordinates:
         x, y = coord
         grid[x][y]['value'] = letters[id]
+        non_infinite_letters.append(letters[id])
         grid[x][y]['is_occupied'] = True
         grid[x][y]['is_new'] = False
         id += 1
 
-    for j in range(grid_width):
-        for i in range(grid_height):
-            print(grid[i][j]['value'], end=" ")
-        print('\n')
-    print('--------------------')
+    # for j in range(grid_width):
+    #     for i in range(grid_height):
+    #         print(grid[i][j]['value'], end=" ")
+    #     print('\n')
+    # print('--------------------')
 
     # Populate grid with original coordinates
-    for _ in range(0, 10):
+    for _ in range(0, 100):
         for j in range(grid_width):
             for i in range(grid_height):
                 if grid[i][j]['is_occupied'] and not grid[i][j]['is_new']:
@@ -113,6 +123,32 @@ def main():
         #     print('\n')
         # print('--------------------')
 
+    # scan border
+    # print(non_infinite_letters)
+    for i in range(grid_width):
+        l1 = grid[i][0]['value']
+        l2 = grid[i][grid_width-1]['value']
+        if l1 in non_infinite_letters:
+            non_infinite_letters.remove(l1)
+        if l2 in non_infinite_letters:
+            non_infinite_letters.remove(l2)
+    for j in range(grid_height):
+        l1 = grid[i][0]['value']
+        l2 = grid[i][grid_height-1]['value']
+        if l1 in non_infinite_letters:
+            non_infinite_letters.remove(l1)
+        if l2 in non_infinite_letters:
+            non_infinite_letters.remove(l2)
+    # print(non_infinite_letters)
 
+    output = dict(zip(non_infinite_letters, [0 for i in range(len(non_infinite_letters))]))
+    for j in range(grid_width - 1):
+        for i in range(grid_height - 1):
+            if(grid[i][j]['value'] in non_infinite_letters):
+                output[grid[i][j]['value']] += 1
+    #     print('\n')
+    # print('--------------------')
+    print(output)
+    print(max(output.values()))
 if __name__ == "__main__":
     main()
