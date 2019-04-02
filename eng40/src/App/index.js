96da8d354935ba33c76b0265c1104a7bbb02c45f
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2019-04-02 14:22:16 -0400

    Initial playground for TS

---
+++ eng40/src/App/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
+
+const App = () => {
+    return <div>Hi.</div>
+}
+
+export default withRouter(App)
