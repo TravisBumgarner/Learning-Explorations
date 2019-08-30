7d44409d8e68d480bd2d0c1ae6cf506f18d34f99
Travis Bumgarner <travisbumgarner@ip-172-18-7-27.us-west-2.compute.internal>
2019-08-30 15:02:14 -0600

    mvp for an arduino library

---
+++ morse/Morse.cpp
@@ -0,0 +1,24 @@
+#include "Arduino.h"
+#include "Morse.h"
+
+Morse::Morse(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+
+void Morse::dot()
+{
+  digitalWrite(_pin, HIGH);
+  delay(250);
+  digitalWrite(_pin, LOW);
+  delay(250);  
+}
+
+void Morse::dash()
+{
+  digitalWrite(_pin, HIGH);
+  delay(1000);
+  digitalWrite(_pin, LOW);
+  delay(250);
+}
