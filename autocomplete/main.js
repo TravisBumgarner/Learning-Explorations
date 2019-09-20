3c146009cd4ce977a9cb249c971bc3b64cb7757c
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-09-20 12:50:54 -0400

    Autcomplete yay

--- autocomplete/main.js
+++ autocomplete/main.js
@@ -1,18 +1,46 @@
-const fetchData = () => {
-  const results = [];
-  const datalist = document.getElementById("autocomplete-results");
-  results.forEach(result => {
-    const node = document.createElement("option");
-    node.value = result;
-    datalist.append(node);
+const fetchGeneralResults = query => {
+  const results = [
+    {
+      text: "python",
+      type: "query"
+    },
+    {
+      text: "python fundamentals",
+      type: "query"
+    },
+    {
+      text: "Advanced Python",
+      type: "course"
+    },
+    {
+      text: "Bob Python",
+      type: "author"
+    }
+  ].filter(({ text }) => text.toLowerCase().includes(query));
+
+  return results;
+};
+
+const updateAutocomplete = event => {
+  const ulResults = document.getElementById("autocomplete-results");
+  ulResults.innerHTML = "";
+
+  const query = event.srcElement.value;
+  if (!query) return;
+
+  const results = fetchGeneralResults(query);
+
+  results.forEach(({ type, text }) => {
+    const node = document.createElement("li");
+    node.innerHTML = `<strong>${text}</strong> [${type}]`;
+    ulResults.append(node);
   });
 };
 
-fetchData();
-
-document.getElementById("autocomplete-input").addEventListener("focus", () => {
+document.getElementById("autocomplete-input").addEventListener("keyup", () => {
+  const autocompleteInput = document.getElementById("autocomplete-input");
   const resultsWrapper = document.getElementById("results-wrapper");
-  resultsWrapper.style.display = "block";
+  resultsWrapper.style.display = autocompleteInput.value ? "block" : "none";
 });
 
 document
@@ -21,3 +49,7 @@ document
     const resultsWrapper = document.getElementById("results-wrapper");
     resultsWrapper.style.display = "none";
   });
+
+document
+  .getElementById("autocomplete-input")
+  .addEventListener("keyup", updateAutocomplete);
