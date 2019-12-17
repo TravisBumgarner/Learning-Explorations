d113f272e27071ae62407c09397cbf1966796308
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2019-12-17 16:24:02 -0500

    whooop

--- cv/main.py
+++ cv/main.py
@@ -1,17 +1,9 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-
-img = cv2.imread('sample.jpg', 0)
-
-LOOK_UP_TABLE = np.array([i for i in range(0,256)])
-if(len(LOOK_UP_TABLE) != 256):
-    raise ValueError()
-
-#         np.array([15,56,15]), #'0f380f' Black
-        # np.array([48,246,35]), #'30f6230' Dark Gray
-        # np.array([139,172,15]), #'8bac0f' Light Gray
-        # np.array([155,188,15]) # '9bbc0f' White
+from imutils.video import VideoStream
+import imutils
+import time
 
 def bucketColors(matrix, distinctColorCount):
     bucket_segments = 255 / distinctColorCount
@@ -19,14 +11,28 @@ def bucketColors(matrix, distinctColorCount):
     return matrix.astype(np.uint8)
     # Not fully typed out. Need to figure out how to convert colors. 
 
-grayscale_buckets = bucketColors(img, 3)
-color_buckets = grayscale_buck`ets[:, :, np.newaxis] * np.ones((667, 1000,3)).astype(np.uint8)
+vs = VideoStream(src=0).start()
 
-color_buckets = np.where(color_buckets == [0,0,0], np.array([155,188,15]),color_buckets)
-color_buckets = np.where(color_buckets == [1,1,1], np.array([48,246,35]),color_buckets)
-color_buckets = np.where(color_buckets == [2,2,2], np.array([139,172,15]),color_buckets)
-color_buckets = np.where(color_buckets == [3,3,3], np.array([15,56,15]),color_buckets)
-cv2.imshow('image', color_buckets.astype(np.uint8))
-cv2.waitKey(0)
+time.sleep(2)
+
+WIDTH = 1000
+HEIGHT = 667
+
+while True:
+    # img = cv2.imread('sample.jpg', 0)
+    img = vs.read()
+    img = imutils.resize(img, height=HEIGHT, width=WIDTH)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayscale_buckets = bucketColors(img, 3)
+    color_buckets = grayscale_buckets[:, :, np.newaxis] * np.ones((562, 1000,3)).astype(np.uint8)
+
+    color_buckets = np.where(color_buckets == [0,0,0], np.array([155,188,15]),color_buckets)
+    color_buckets = np.where(color_buckets == [1,1,1], np.array([48,246,35]),color_buckets)
+    color_buckets = np.where(color_buckets == [2,2,2], np.array([139,172,15]),color_buckets)
+    color_buckets = np.where(color_buckets == [3,3,3], np.array([15,56,15]),color_buckets)
+    cv2.imshow('image', color_buckets.astype(np.uint8))
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
 cv2.destroyAllWindows()
 
