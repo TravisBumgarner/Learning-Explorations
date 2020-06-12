584412c574532a2ceb0481e9896a07617a6b65bc
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-06-12 00:16:41 -0400

    clearing repo to avoid security alerts

--- coding_practice/test_new_years_chaos.py
+++
@@ -1,8 +0,0 @@
-from .new_years_chaos import minimumBribes
-
-def test():
-    assert(minimumBribes([1,2,3]) == 0)
-    assert(minimumBribes([1,3,2]) == 1)
-    assert(minimumBribes([3,1,2]) == 2)
-    assert(minimumBribes([4,1,2,3]) == "Too Chaotic")
-
