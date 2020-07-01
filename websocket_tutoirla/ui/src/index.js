2a8f62508677448af5ff825428c83f7391094d7b
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-07-01 14:46:37 -0400

    Added styling

--- websocket_tutoirla/ui/src/index.js
+++ websocket_tutoirla/ui/src/index.js
@@ -1,8 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    margin: 20vh 20vw;
+    width: 60vw;
+    height: 60vw;
+    display: flex;
+    flex-direction: column;
+`
+
+const ChatBox = styled.input`
+    width: 100%;
+    box-sizing: border-box;
+`
+
+const ChatMessage = styled.div`
+`
+
+const ReceivedMessages = styled.div`
+    width: 100%;
+    flex-grow: 1;
+    background-color: rgba(0,0,0,0.2);
+`
 
 const App = () => {
-    return <p>Hello!</p>
+    return <Wrapper>
+        <ReceivedMessages>
+            <ChatMessage>ChatMessage</ChatMessage>
+            <ChatMessage>ChatMessage</ChatMessage>
+            <ChatMessage>ChatMessage</ChatMessage>
+        </ReceivedMessages>
+        <ChatBox />
+    </Wrapper>
 }
 
 ReactDOM.render(
