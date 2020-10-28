e8749cdc6f7d0194125c565226f06a1a86e42fc8
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-10-28 13:56:33 -0400

    setup read and write endpoints with an express server

--- fb-demo/server.js
+++ fb-demo/server.js
@@ -1,9 +1,12 @@
 import { config } from 'dotenv';
-config();
 import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
 import "firebase/database";
+import express from 'express'
+import bodyParser from 'body-parser'
+
+config();
 
 var firebaseConfig = {
     apiKey: process.env.API_KEY,
@@ -20,12 +23,32 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-function writeUserData(userId, name, email, imageUrl) {
+function writeUserData(userId, name) {
     firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture: imageUrl
+        username: name
     });
 }
 
-writeUserData('asdadasd', 'bar', 'buzz', 'dfsd')
+const app = express();
+
+app.get('/create/:userid/:username', function (req, res) {
+    writeUserData(req.params.userid, req.params.username)
+    res.send('done')
+})
+
+app.get('/username/:user', function (req, res) {
+    console.log(req.params.user)
+    firebase.database().ref('/users/' + req.params.user).once('value').then(function (snapshot) {
+        var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        console.log(username)
+        res.send(username)
+    });
+
+})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const port = 8000
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
