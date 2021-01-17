e2419f1b06d50deae2fc9a682ce783002d998cfe
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-01-16 23:55:07 -0500

    started collecting notes

---
+++ jspractice/tips.js
@@ -0,0 +1,16 @@
+
+// Reduce
+
+x.reduce((accumulator, currentValue) => {
+    // do things
+    return accumulator
+}, {})
+
+// Sort
+
+/*
+    compareFunction(a, b) returns
+        < 0 -> a comes before b
+        = 0 -> no change
+        > 0 -> b comes before a
+*/
