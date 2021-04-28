3c81b4a2545fe414c906663dde95bb7b9810acbc
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-04-28 11:54:51 -0400

    Cloudinary initial commit

---
+++ cloudy/ui/src/App/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const App = () => {
+    return <div>Hello World!</div>
+}
+
+export default App
---
+++ cloudy/ui/src/theme.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        font-size: 16px;
+        font-weight: 400;
+    }
+`
+
+export {
+    GlobalStyle
+}
