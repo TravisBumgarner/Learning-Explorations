d2adc622ea06351d92e0e10f45f77729becef63d
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-12-17 15:20:56 -0500

    adding today's learnings

---
+++ aws-stuffs/js-aws/sqs_listqueues.js
@@ -0,0 +1,19 @@
+//docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sqs-examples-using-queues.html
+
+// Load the AWS SDK for Node.js
+https: var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "us-west-2" });
+
+// Create an SQS service object
+var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+
+var params = {};
+
+sqs.listQueues(params, function (err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.QueueUrls);
+  }
+});
