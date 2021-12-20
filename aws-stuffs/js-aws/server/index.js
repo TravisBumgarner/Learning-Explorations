e04bd472d7220a6935ac0ce47c49ef8b0fcac9d6
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-12-20 16:09:50 -0500

    More cleanup before writing api

---
+++ aws-stuffs/js-aws/server/index.js
@@ -0,0 +1,7 @@
+const app = require('./src/app')
+
+const port = 5001
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
--- aws-stuffs/js-aws/server/src/app.js
+++ aws-stuffs/js-aws/server/src/app.js
@@ -2,12 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5001
 
-
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = app
