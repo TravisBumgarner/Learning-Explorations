752b15166221abbc9a143b9f4df5b8980e0a89e7
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-12-27 11:29:10 -0700

    setup basic redis set and get

--- js-redis/backend/src/app.ts
+++ js-redis/backend/src/app.ts
@@ -4,10 +4,18 @@ import * as redis from './redis'
 
 const app = express()
 
-
 app.get('/', async (req: express.Request, res: express.Response) => {
-  const data = await redis.get()
-  res.send(data)
+  res.send('pong')
+})
+
+app.get('/redis/set/:key/:value', async (req: express.Request, res: express.Response) => {
+  const redisResponse = await redis.set(req.params.key, req.params.value)
+  res.send(redisResponse)
+})
+
+app.get('/redis/get/:key/', async (req: express.Request, res: express.Response) => {
+  const redisResponse = await redis.get(req.params.key)
+  res.send(redisResponse)
 })
 
 export default app
--- js-redis/backend/src/redis/index.ts
+++ js-redis/backend/src/redis/index.ts
@@ -1,17 +1,27 @@
 import { createClient } from 'redis';
 
-const get = async () => {
+const connect = async () => {
     const client = createClient();
-
     client.on('error', (err) => console.log('Redis Client Error', err));
-
     await client.connect();
 
-    await client.set('key', 'value');
-    const value = await client.get('key');
+    return client
+}
+
+const set = async (key: string, value: string) => {
+    const client = await connect()
+    await client.set(key, value)
+}
+
+const get = async (key: string) => {
+    const client = await connect()
+
+    const value = await client.get(key);
     return value
 }
 
 export {
-    get
+    get,
+    set,
+    connect
 }
