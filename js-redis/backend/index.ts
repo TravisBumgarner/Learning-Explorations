687211afa90fc31c54bbbda325ee083ab1d77754
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-12-27 12:40:03 -0700

    can send and get messages from redis

--- js-redis/backend/index.ts
+++ js-redis/backend/index.ts
@@ -6,17 +6,29 @@ const server = createServer()
 
 let wsServer = new WSServer({ server })
 
+const NEW_MESSAGES = 'newMessages'
+const CURRENT_MESSAGES = 'currentMessages'
+
 wsServer.on('connection', async (ws) => {
   const subscriber = await createClient()
   await subscriber.connect();
 
-  await subscriber.subscribe('foo', (message) => {
+  const publisher = await createClient()
+  await publisher.connect()
+
+  const currentMessages = await subscriber.lRange(CURRENT_MESSAGES, 0, -1)
+  await ws.send(JSON.stringify(currentMessages));
+
+  await subscriber.subscribe(NEW_MESSAGES, (message) => {
     ws.send(JSON.stringify(message));
   });
 
-  ws.on('message', (message) => {
-    console.log(`received: ${message}`);
-    ws.send(JSON.stringify(message));
+  ws.on('message', async (message) => {
+    const [_a, _b] = await publisher
+      .multi()
+      .publish(NEW_MESSAGES, message)
+      .rPush(CURRENT_MESSAGES, message)
+      .exec();
   });
 });
 
