9a3b6273ca2cad44d161af26d9d490bbac6856a4
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-01-07 14:43:01 -0700

    added boilerplate to front end

---
+++ apache-kafka/frontend/src/App/App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+import { Body, Title } from 'sharedComponents'
+import { ColorCounts } from '../../../sharedTypes'
+
+const Button = styled.button`
+  ${({ color }) => `
+    color: ${color};
+`}`
+
+const DEFAULT_COLOR_COUNTS = {
+  red: 0,
+  green: 0,
+  blue: 0
+}
+
+const App = () => {
+  const [counts, setCounts] = React.useState<ColorCounts>(DEFAULT_COLOR_COUNTS)
+
+  const Counts = Object.keys(counts).map((color: keyof ColorCounts) => <li>{color} - {counts[color]}</li>)
+  return (
+    <Body>
+      <div>
+        <Title>Click Buttons with Kafka</Title>
+        <p>Refresh to increase button count.</p>
+        <Button color="red">Red</Button>
+        <Button color="green">Green</Button>
+        <Button color="blue">Blue</Button>
+        <p>Click counts</p>
+        <ul>
+          {Counts}
+        </ul>
+      </div>
+    </Body>
+  )
+}
+
+export default App
