1133a36151240c2b79e656df17e994cf6c95ec30
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-01-25 12:31:56 -0700

    refactored a bit

--- archives/2022-01/technology-io-ts/sandbox/src/index.ts
+++ archives/2022-01/technology-io-ts/sandbox/src/index.ts
@@ -1,23 +1,24 @@
 import * as t from 'io-ts'
 import { pipe } from 'fp-ts/function'
 import { fold } from 'fp-ts/Either'
+import { string } from 'fp-ts'
 
-const UserCodec = t.type({
+const userCodec = t.type({
     userId: t.number,
     name: t.string
 })
-type UserCodec = t.TypeOf<typeof UserCodec>
+type UserCodec = t.TypeOf<typeof userCodec>
 
 type DecodeResponseParams = {
     codec: t.TypeC<any>,
     rawResponse: any,
-    onError?: (rawResponse?: any) => void,
-    onSuccess?: (rawResponse?: any) => void
+    onError: (e: Array<t.ValidationError>) => (void | undefined),
+    onSuccess: (parsedResponse: t.TypeC<any>) => (void | undefined)
 }
 
 const decodeResponse = (params: DecodeResponseParams) => {
-    const onError = params.onError || function (rawResponse: any) { console.error(`Failed on ${JSON.stringify(rawResponse)}`) }
-    const onSuccess = params.onSuccess || function (rawResponse: any) { console.log(`Succeeded on ${JSON.stringify(rawResponse)}`) }
+    const onError = params.onError
+    const onSuccess = params.onSuccess
 
     return pipe(params.codec.decode(params.rawResponse), fold(onError, onSuccess))
 }
@@ -41,8 +42,10 @@ const fakeUserAPIResponses = [
 const main = () => {
     fakeUserAPIResponses.forEach(rawResponse => {
         decodeResponse({
-            codec: UserCodec,
-            rawResponse: rawResponse
+            codec: userCodec,
+            rawResponse: rawResponse,
+            onError: (e) => console.log('boo error'),
+            onSuccess: (data) => { console.log('woo data' + JSON.stringify(data)) }
         })
     })
 }
