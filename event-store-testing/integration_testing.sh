1854f96308e79af4e258770b73db0b6bf2dc9d7c
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-01-26 10:35:06 -0700

    Adding Kathys suggestions

--- event-store-testing/integration_testing.sh
+++ event-store-testing/integration_testing.sh
@@ -1,14 +1,23 @@
+FILE=".env"
+
+if (( $# == 1 ))
+then
+    FILE=".env.$1"
+fi
+
+echo "Env file: $FILE"
+
 function wait_eventstore_db() {
     sleep 1
     while true; do
-        if docker-compose ps | egrep 'eventstore.+Exit'
+        if docker-compose --env-file $FILE ps | egrep 'eventstore.+Exit'
         then
             echo "EventStoreDB failed to startup, see log."
             exit 1
-        elif docker-compose ps | egrep 'eventstore.*Up \(health: starting\)'
+        elif docker-compose --env-file $FILE ps | egrep 'eventstore.*Up \(health: starting\)'
         then
             echo "EventStoreDB Starting"
-        elif docker-compose ps | egrep 'eventstore.*Up \(healthy\)'
+        elif docker-compose --env-file $FILE ps | egrep 'eventstore.*Up \(healthy\)'
         then
             echo "EventStoreDB Running"
             break
@@ -16,7 +25,8 @@ function wait_eventstore_db() {
         sleep 1
     done
 }
-npm run eventstore-testing
+
+docker-compose --env-file $FILE up -d
 wait_eventstore_db
-npm run test-integration-jest
-docker-compose stop eventstore.db
+./node_modules/jest/bin/jest.js --config=jest.integration.js --detectOpenHandles
+docker-compose --env-file $FILE stop eventstore.db
