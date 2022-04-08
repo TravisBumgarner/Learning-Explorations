1e262ee5ea4b5f65b11160374e813979aeb30454
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-04-08 13:08:53 +0200

    fun weather demo

--- technology-ink2/2-why-go-outside/src/cli.tsx
+++ technology-ink2/2-why-go-outside/src/cli.tsx
@@ -1,10 +1,75 @@
 import React from 'react';
 import { render, Text } from 'ink';
+import axios from 'axios';
+require('dotenv').config()
+
+const availableCities: Record<string, { lat: number, lon: number }> = {
+    home: {
+        lat: 42.360081,
+        lon: -71.058884
+    }
+}
+
+const getWeather = async (city) => {
+    let response: string
+
+    if (city in availableCities) {
+        const { lat, lon } = availableCities[city]
+        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`
+        const apiResponse = await axios.get(URL)
+        const currentWeather = (apiResponse.data.weather.map(({ description }) => description).join(', '))
+        response = `Outside it's ${currentWeather}. Stay inside!`
+    } else {
+        response = "Why would you need weather elsewhere?"
+    }
+    return response
+}
 
 type TypeScriptDemoProps = {
     language: string
 }
 
-const Demo = ({ language }: TypeScriptDemoProps) => <Text>Hello {language} World</Text>;
+const Loading = () => {
+    const [dots, setDots] = React.useState<number>(0)
+    const [color, setColor] = React.useState<string>('green')
+
+    React.useEffect(() => {
+        if (dots < 5) {
+            setColor('green')
+        } else if (dots >= 5 && dots < 10) {
+            setColor('yellow')
+        } else {
+            setColor('red')
+        }
+
+    }, [dots])
+
+    React.useEffect(() => {
+        const intervalId = setInterval(() => setDots(prev => prev + 1), 50)
+        return () => clearInterval(intervalId)
+    }, [])
+
+    return <Text color={color}>Fetching weather, please hold{'.'.repeat(dots)}</Text>
+}
+
+const Demo = ({ language }: TypeScriptDemoProps) => {
+    const [isLoading, setIsLoading] = React.useState<boolean>(true)
+    const [weather, setWeather] = React.useState<string>(null)
+
+    React.useEffect(() => {
+        getWeather('home').then(r => {
+            setWeather(r)
+            setTimeout(() => setIsLoading(false), 1000) // Just to add dramatic timing for <Loading /> animation to shine!
+        })
+    })
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    return <Text>
+        {weather}
+    </Text>
+}
 
 render(<Demo language="TypeScript" />);
