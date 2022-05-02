c23f57e1dc49d2539282287f336bfcece34bf0e6
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-02 17:09:52 +0200

    Working prototype

--- technology-eventstore-v2/src/eventHandler.ts
+++ technology-eventstore-v2/src/eventHandler.ts
@@ -1,27 +1,48 @@
-import { connectHandlerToStreamEvents } from './eventstore'
 import { createConnection, getRepository } from 'typeorm'
+import { SubscribeToAllOptions} from "@eventstore/db-client"
 
+import { connectHandlerToStreamEvents } from './eventstore'
 import ormconfig from './db/ormconfig'
 import entity from './db'
+import { ReadPosition } from '@eventstore/db-client'
 
 BigInt.prototype["toJSON"] = function () {
     return this.toString();
 };
 
 const eventHandler = async () => {
-    await createConnection(ormconfig)
-    connectHandlerToStreamEvents(e => {
-        if (e.commitPosition) {
-            const projectionRepository = getRepository(entity.ProjectionOffset)
+    const postgresConnection = await createConnection(ormconfig)
 
-            const projectionOffset = new entity.ProjectionOffset
-            // Because :shrug: https://stackoverflow.com/questions/59927625/how-to-store-big-int-in-nest-js-using-typeorm
-            projectionOffset.commitPosition = e.commitPosition
-            projectionRepository.save(projectionOffset)
-        } else {
-            console.log('Event does not have commitPosition')
+    const result = await postgresConnection
+        .getRepository(entity.ProjectionOffset)
+        .createQueryBuilder('ProjectionOffset')
+        .select('MAX(commit_position)', 'commitPosition')
+        .getRawOne<{commitPosition: bigint}>()
+
+
+    const options: SubscribeToAllOptions = result && result.commitPosition
+        ? {
+            fromPosition: {
+                commit: result.commitPosition,
+                prepare: result.commitPosition
+            }
         }
+        : {
+            fromPosition: 'start'
+        }
+console.log(options)
 
-    })
+connectHandlerToStreamEvents(options, event => {
+    if (event.commitPosition) {
+        const projectionRepository = getRepository(entity.ProjectionOffset)
+
+        const projectionOffset = new entity.ProjectionOffset
+        projectionOffset.commit_position = event.commitPosition
+        projectionRepository.save(projectionOffset)
+    } else {
+        console.log('Event does not have commitPosition')
+    }
+
+})
 }
 eventHandler()
--- technology-eventstore-v2/src/eventstore.ts
+++ technology-eventstore-v2/src/eventstore.ts
@@ -1,11 +1,11 @@
-import { EventStoreDBClient, ResolvedEvent } from '@eventstore/db-client'
+import { EventStoreDBClient, ResolvedEvent, SubscribeToAllOptions } from '@eventstore/db-client'
 
 export const client = new EventStoreDBClient({endpoint: 'localhost:2113'}, { insecure: true })
 
 type eventHandler = (e: ResolvedEvent) => void
 
-export const connectHandlerToStreamEvents = async (handler: eventHandler): Promise<void> => {
-    client.subscribeToAll({ fromPosition: 'start' }).on('data', handler)
+export const connectHandlerToStreamEvents = async (options: SubscribeToAllOptions, handler: eventHandler): Promise<void> => {
+    client.subscribeToAll(options).on('data', handler)
 }
 
 export default client
