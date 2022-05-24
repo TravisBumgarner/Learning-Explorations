a71b34d286d0c984ed9f1e8b4fece5e4aa8599ee
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-24 13:13:44 +0200

    backing up

--- technology-indexeddb/src/App/App.tsx
+++ technology-indexeddb/src/App/App.tsx
@@ -1,13 +1,67 @@
-import * as React from 'react'
+import React from 'react'
+import {db} from '../db'
+import {useLiveQuery} from 'dexie-react-hooks'
 
-import { Body, Title } from 'sharedComponents'
+function AddFriendForm() {
+  const [name, setName] = React.useState("");
+  const [age, setAge] = React.useState(5);
+  const [status, setStatus] = React.useState("");
 
-const App = () => {
-  return (
-    <Body>
-      <Title>Hello World!</Title>
-    </Body>
-  )
+  async function addFriend() {
+    try {
+
+      // Add the new friend!
+      const id = await db.friends.add({
+        name,
+        age
+      });
+
+      setStatus(`Friend ${name} successfully added. Got id ${id}`);
+      setName("");
+      setAge(5);
+    } catch (error) {
+      setStatus(`Failed to add ${name}: ${error}`);
+    }
+  }
+
+  return <>
+    <p>
+      {status}
+    </p>
+    Name:
+    <input
+      type="text"
+      value={name}
+      onChange={ev => setName(ev.target.value)}
+    />
+    Age:
+    <input
+      type="number"
+      value={age}
+      onChange={ev => setAge(Number(ev.target.value))}
+    />
+    
+    <button onClick={addFriend}>
+      Add
+    </button>
+  </>
 }
+
+function FriendList() {
+  const friends = useLiveQuery(
+    () => db.friends.toArray()
+  );
+
+  return <ul>
+    {friends?.map(friend => <li key={friend.id}>
+      {friend.name}, {friend.age}
+    </li>)}
+  </ul>;
+}
+
+const App = () => (<div>
+  <AddFriendForm />
+  <FriendList />
+</div>)
 
 export default App
