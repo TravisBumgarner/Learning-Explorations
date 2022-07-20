c66ce290f829af4c933c47ff1de632c6a20987f1
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-07-20 18:08:08 -0400

    moved technology-mongodb to poc

--- technology-mongodb/backend/index.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-import app from './src/app'
-
-const port = 5001
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
