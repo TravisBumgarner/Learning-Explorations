ccd56bf4033ef14a36b886ed679917a79b5fd3c5
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-08-08 13:15:27 -0600

    tutorial result

--- technology-slack-aws-lambda/slack-and-pulumi/index.ts
+++ technology-slack-aws-lambda/slack-and-pulumi/index.ts
@@ -3,7 +3,20 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket", {
+    website: {
+        indexDocument: "index.html",
+    },
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    acl: "public-read",
+    contentType: "text/html",
+    bucket: bucket,
+    source: new pulumi.asset.FileAsset("index.html")
+});
+
+export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`
