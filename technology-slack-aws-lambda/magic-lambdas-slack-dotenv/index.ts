fc9281b49356c6d7b665a7b954dfc7ff1a3dc605
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-08-08 15:04:26 -0600

    changed output

--- technology-slack-aws-lambda/magic-lambdas-slack-dotenv/index.ts
+++ technology-slack-aws-lambda/magic-lambdas-slack-dotenv/index.ts
@@ -10,7 +10,50 @@ docsBucket.onObjectCreated("docsHandler", (e) => {
     axios.post(
         config.webhookSlackURL,
         {
-            "text":`Document created at ${new Date()}`
+            "text": JSON.stringify(e)
         }
     )
 });
+
+/*
+Output ->
+
+{
+    "Records": [
+        {
+            "eventVersion": "2.1",
+            "eventSource": "aws:s3",
+            "awsRegion": "us-east-1",
+            "eventTime": "2022-08-08T21:03:03.144Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {
+                "principalId": "id"
+            },
+            "requestParameters": {
+                "sourceIPAddress": "ip.ip.ip"
+            },
+            "responseElements": {
+                "x-amz-request-id": "id",
+                "x-amz-id-2": "id2"
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "id",
+                "bucket": {
+                    "name": "magic-lambdas-abc123",
+                    "ownerIdentity": {
+                        "principalId": "id"
+                    },
+                    "arn": "arn:aws:s3:::magic-lambdas-abc123"
+                },
+                "object": {
+                    "key": "2022-08-03+10.00.58.jpg",
+                    "size": 4684648,
+                    "eTag": "dead-beaf",
+                    "sequencer": "12345"
+                }
+            }
+        }
+    ]
+}
+*/
