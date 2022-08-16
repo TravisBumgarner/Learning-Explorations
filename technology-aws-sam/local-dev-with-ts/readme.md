03aa9a6efd2c695e5d7055c06287789bbee1a353
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-08-16 12:21:13 -0600

    finished ts demo

---
+++ technology-aws-sam/local-dev-with-ts/readme.md
@@ -0,0 +1,1 @@
+https://www.toptal.com/aws/typescript-jest-aws-sam-tutorial
---
+++ technology-aws-sam/local-dev-with-ts/src/test-lambda/app.ts
@@ -0,0 +1,10 @@
+import { APIGatewayEvent } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayEvent) => {
+    console.log('incoming event is', JSON.stringify(event));
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Request was successful!' })
+    };
+    return response;
+};
