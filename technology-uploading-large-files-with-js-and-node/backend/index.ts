8d548e3f796c048498e36396e39fa03c541c1f5b
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-09-08 14:21:40 -0600

    refactored to ts

--- technology-uploading-large-files-with-js-and-node/backend/index.ts
+++ technology-uploading-large-files-with-js-and-node/backend/index.ts
@@ -1,34 +1,31 @@
-const express = require('express')
-const multiparty = require('multiparty')
-const fs = require('fs')
-const path = require('path')
-const { Buffer } = require('buffer')
-const cors = require('cors')
+import fs from 'fs'
+import path from 'path'
+import { Buffer } from 'buffer'
 
-// file path
+import cors from 'cors'
+import express from 'express'
+import multiparty from 'multiparty'
+
 const STATIC_FILES = path.join(__dirname, './static/files')
-// Temporary path to upload files
 const STATIC_TEMPORARY = path.join(__dirname, './static/temporary')
-const server = express()
 
-server.use(cors({
-    origin: '*'
-}));
+const app = express()
 
-// Static file hosting
-server.use(express.static(path.join(__dirname, './dist')))
-// Interface for uploading slices
+app.use(cors({ origin: '*' }));
 
-server.post('/upload', (req, res) => {
+app.use(express.static(path.join(__dirname, './dist')))
+
+app.post('/upload', (req, res) => {
     const form = new multiparty.Form();
+
     form.parse(req, function (err, fields, files) {
         let filename = fields.filename[0]
         let hash = fields.hash[0]
         let chunk = files.chunk[0]
         let dir = `${STATIC_TEMPORARY}/${filename}`
-        // console.log(filename, hash, chunk)
+
         try {
-            if (!fs.existsSync(dir)) fs.mkdirSync(dir)
+            if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true })
             const buffer = fs.readFileSync(chunk.path)
             const ws = fs.createWriteStream(`${dir}/${hash}`)
             ws.write(buffer)
@@ -41,27 +38,32 @@ server.post('/upload', (req, res) => {
     })
 })
 
-//Merged slice interface
-server.get('/merge', async (req, res) => {
+app.get('/merge', async (req, res) => {
     const { filename } = req.query
+    const tempDir = path.join(STATIC_TEMPORARY, filename as string)
+    
     try {
         let len = 0
-        const bufferList = fs.readdirSync(`${STATIC_TEMPORARY}/${filename}`).map((hash, index) => {
-            const buffer = fs.readFileSync(`${STATIC_TEMPORARY}/${filename}/${index}`)
+        const bufferList = fs.readdirSync(tempDir).map((_, hash) => {
+            const tempFile = path.join(tempDir, String(hash))
+            const buffer = fs.readFileSync(tempFile)
             len += buffer.length
             return buffer
         });
         //Merge files
         const buffer = Buffer.concat(bufferList, len);
+        if (!fs.existsSync(STATIC_FILES)) fs.mkdirSync(STATIC_FILES, { recursive: true })
         const ws = fs.createWriteStream(`${STATIC_FILES}/${filename}`)
         ws.write(buffer);
         ws.close();
         res.send(`Section merge completed`);
+
+        fs.rmSync(tempDir, {recursive: true, force: true})
     } catch (error) {
         console.error(error);
     }
 })
 
-server.listen(3000, _ => {
-    console.log('http://localhost:3000/')
+app.listen(5001, () => {
+    console.log('http://localhost:5001/')
 })
