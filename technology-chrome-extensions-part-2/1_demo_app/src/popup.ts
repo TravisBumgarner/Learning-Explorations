7d32082d49a933e20062cf56faa4215e62283b56
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-10-03 12:09:27 -0600

    a bunch of code that doesn't quite work lol

--- technology-chrome-extensions-part-2/1_demo_app/src/popup.ts
+++ technology-chrome-extensions-part-2/1_demo_app/src/popup.ts
@@ -1,61 +1,82 @@
-const recordButton = document.getElementById('record')
+import Dexie, { Table } from 'dexie'
+import { v4 as uuidv4 } from 'uuid'
 
-recordButton.addEventListener('click', () => {
-  alert('hi')
-})
+type TRecording = {
+  id: string
+  file: Blob
+  name: string
+}
+class MySubClassedDexie extends Dexie {
+  videos!: Table<TRecording>
 
-// const handleStream = () => {
-    
-// }
+  constructor() {
+    super('recordings')
+    this.version(1).stores({
+      videos: '[id]',
+    })
+  }
+}
 
-// recordButton.addEventListener('click', () => {
-//     chrome.tabCapture.capture({
-//         video: true, audio: true,
-//         videoConstraints: {
-//           mandatory: {
-//             minWidth: 16,
-//             minHeight: 9,
-//             maxWidth: 854,
-//             maxHeight: 480,
-//             maxFrameRate: 60,  // 0 ~ 60
-//           },
-//         },
-//       },
-// })
+type RecordingGlobals = {
+  mediaRecorder: MediaRecorder | null,
+  mediaChunks: Blob[],
+  mediaStream: MediaStream | null,
+  recordingButton: HTMLElement
+  stopButton: HTMLElement
+}
 
-console.log('hi')
+const recordingGlobals: RecordingGlobals = {
+  mediaRecorder: null,
+  mediaChunks: [],
+  mediaStream: null,
+  recordingButton: document.getElementById('record'),
+  stopButton: document.getElementById('stop')
+}
 
+const db = new MySubClassedDexie()
 
+const record = (stream: MediaStream) => {
+  recordingGlobals.mediaStream = stream
+  recordingGlobals.mediaRecorder = new MediaRecorder(stream);
+  recordingGlobals.mediaRecorder.ondataavailable = ({ data }) => recordingGlobals.mediaChunks.push(data);
+  recordingGlobals.mediaRecorder.start();
+  chrome.action.setBadgeText({ text: 'Rec' })
+}
 
-// var receiver = null;
-// function playCapturedStream(stream) {
-//   if (!stream) {
-//     console.error('error: ' + chrome.runtime.lastError.message);
-//     return;
-//   }
-//   if (receiver != null) {
-//     receiver.close();
-//   }
-//   receiver = window.open('receiver.html');
-//   receiver.currentStream = stream;
-// }
+const stopRecording = () => {
+  const blob = new Blob(recordingGlobals.mediaChunks);
+  recordingGlobals.mediaRecorder.stop();
+  recordingGlobals.mediaStream && recordingGlobals.mediaStream.getTracks().forEach((track) => track.stop());
+  recordingGlobals.mediaChunks = [];
 
-// function testCapture() {
-//   chrome.tabCapture.capture({
-//     video: true, audio: true,
-//     videoConstraints: {
-//       mandatory: {
-//         minWidth: 16,
-//         minHeight: 9,
-//         maxWidth: 854,
-//         maxHeight: 480,
-//         maxFrameRate: 60,  // 0 ~ 60
-//       },
-//     },
-//   },
-//   playCapturedStream);
-// }
+  recordingGlobals.mediaStream.getTracks().forEach(track => track.stop())
+  chrome.action.setBadgeText({ text: 'done' })
+  db.videos.add({
+    id: uuidv4(),
+    file: blob,
+    name: uuidv4(),
+  }).then(() => {
+    console.log('added to db')
+  })
+};
 
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   testCapture();
-// });
+const startRecording = () => {
+  chrome.tabCapture.capture({
+    video: true, audio: true,
+    videoConstraints: {
+      mandatory: {
+        minWidth: 16,
+        minHeight: 9,
+        maxWidth: 854,
+        maxHeight: 480,
+        maxFrameRate: 60,  // 0 ~ 60
+      },
+    },
+  },
+    record
+  )
+}
+
+chrome.action.setBadgeText({ text: '' })
+recordingGlobals.recordingButton.addEventListener('click', startRecording)
+recordingGlobals.stopButton.addEventListener('click', stopRecording)
