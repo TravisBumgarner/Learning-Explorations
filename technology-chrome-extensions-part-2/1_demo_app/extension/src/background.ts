b34a30f11932fcdbfc345777366f27fe1ca27986
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-10-05 16:12:24 -0600

    Deadend :(

--- technology-chrome-extensions-part-2/1_demo_app/extension/src/background.ts
+++ technology-chrome-extensions-part-2/1_demo_app/extension/src/background.ts
@@ -1,128 +1,128 @@
-// import Dexie, { Table } from 'dexie'
-// import { v4 as uuidv4 } from 'uuid'
+import Dexie, { Table } from 'dexie'
+import { v4 as uuidv4 } from 'uuid'
 
 import {
     Message
 } from '../../shared/types'
 
+import { Action } from './types'
+
+let tabIdToRecord: string | null
+let recordingIntervalId: NodeJS.Timeout | null = null
+var mediaRecorder = '';
+var width = 1920;
+var height = 1080;
+var fps = 60;
+
+type TRecording = {
+    id: string
+    file: Blob
+    name: string
+}
+class MySubClassedDexie extends Dexie {
+    videos!: Table<TRecording>
+
+    constructor() {
+        super('recordings')
+        this.version(1).stores({
+            videos: '[id]',
+        })
+    }
+}
+
+type RecordingGlobals = {
+    mediaRecorder: MediaRecorder | null,
+    mediaChunks: Blob[],
+    mediaStream: MediaStream | null,
+}
+
+const recordingGlobals: RecordingGlobals = {
+    mediaRecorder: null,
+    mediaChunks: [],
+    mediaStream: null,
+}
+
+const db = new MySubClassedDexie()
+
+const record = (stream: MediaStream) => {
+    let counter = 0
+    recordingIntervalId = setInterval(() => {
+        chrome.action.setBadgeText({ text: `${counter}` })
+        counter += 1
+    }, 1000)
+
+    recordingGlobals.mediaStream = stream
+    recordingGlobals.mediaRecorder = new MediaRecorder(stream);
+    recordingGlobals.mediaRecorder.ondataavailable = ({ data }) => recordingGlobals.mediaChunks.push(data);
+    recordingGlobals.mediaRecorder.start();
+    chrome.action.setBadgeText({ text: 'Rec' })
+}
+
+const stopRecording = () => {
+    const blob = new Blob(recordingGlobals.mediaChunks);
+    recordingGlobals.mediaRecorder.stop();
+    recordingGlobals.mediaStream && recordingGlobals.mediaStream.getTracks().forEach((track) => track.stop());
+    recordingGlobals.mediaChunks = [];
+
+    clearInterval(recordingIntervalId)
+
+    recordingGlobals.mediaStream.getTracks().forEach(track => track.stop())
+    chrome.action.setBadgeText({ text: 'done' })
+    db.videos.add({
+        id: uuidv4(),
+        file: blob,
+        name: uuidv4(),
+    }).then(() => {
+        console.log('added to db')
+    })
+};
+
+const startRecording = () => {
+    // chrome.tabs.query({ currentWindow: true }, function (tab) {
+    chrome.tabs.getCurrent(function (tab) {
+        console.log(chrome.tabCapture)
+        chrome.tabCapture.capture({
+            video: true, audio: true,
+            videoConstraints: {
+                mandatory: {
+                    minWidth: 16,
+                    minHeight: 9,
+                    maxWidth: 854,
+                    maxHeight: 480,
+                    maxFrameRate: 60,  // 0 ~ 60
+                },
+            },
+        },
+            record
+        )
+    })
+}
+
 chrome.runtime.onMessageExternal.addListener(
     function (request: Message, sender, sendResponse) {
+
         if (sender.origin !== "http://localhost:3000") {
             sendResponse("Nahh")
             return
         }
+        console.log(sender)
+        // tabIdToRecord = sender.tab.id
 
         switch (request.type) {
             case "START_RECORDING": {
                 console.log("Starting recording")
+                startRecording()
                 break
             }
             case "STOP_RECORDING": {
                 console.log("Stopping recording")
+                stopRecording()
                 break
             }
         }
 
         sendResponse('hello from background')
     });
-
-// import { Action } from './types'
-
-// var mediaRecorder = '';
-// var width = 1920;
-// var height = 1080;
-// var fps = 60;
-
-// type TRecording = {
-//     id: string
-//     file: Blob
-//     name: string
-// }
-// class MySubClassedDexie extends Dexie {
-//     videos!: Table<TRecording>
-
-//     constructor() {
-//         super('recordings')
-//         this.version(1).stores({
-//             videos: '[id]',
-//         })
-//     }
-// }
-
-// type RecordingGlobals = {
-//     mediaRecorder: MediaRecorder | null,
-//     mediaChunks: Blob[],
-//     mediaStream: MediaStream | null,
-// }
-
-// const recordingGlobals: RecordingGlobals = {
-//     mediaRecorder: null,
-//     mediaChunks: [],
-//     mediaStream: null,
-// }
-
-// const db = new MySubClassedDexie()
-
-// const record = (stream: MediaStream) => {
-//     recordingGlobals.mediaStream = stream
-//     recordingGlobals.mediaRecorder = new MediaRecorder(stream);
-//     recordingGlobals.mediaRecorder.ondataavailable = ({ data }) => recordingGlobals.mediaChunks.push(data);
-//     recordingGlobals.mediaRecorder.start();
-//     chrome.action.setBadgeText({ text: 'Rec' })
-// }
-
-// const stopRecording = () => {
-//     const blob = new Blob(recordingGlobals.mediaChunks);
-//     recordingGlobals.mediaRecorder.stop();
-//     recordingGlobals.mediaStream && recordingGlobals.mediaStream.getTracks().forEach((track) => track.stop());
-//     recordingGlobals.mediaChunks = [];
-
-//     recordingGlobals.mediaStream.getTracks().forEach(track => track.stop())
-//     chrome.action.setBadgeText({ text: 'done' })
-//     db.videos.add({
-//         id: uuidv4(),
-//         file: blob,
-//         name: uuidv4(),
-//     }).then(() => {
-//         console.log('added to db')
-//     })
-// };
-
-// const startRecording = () => {
-//     chrome.tabCapture.capture({
-//         video: true, audio: true,
-//         videoConstraints: {
-//             mandatory: {
-//                 minWidth: 16,
-//                 minHeight: 9,
-//                 maxWidth: 854,
-//                 maxHeight: 480,
-//                 maxFrameRate: 60,  // 0 ~ 60
-//             },
-//         },
-//     },
-//         record
-//     )
-// }
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     console.log('message received')
-//     console.log(request)
-
-//     switch (request.type as Action) {
-//         case Action.StartRecording: {
-//             startRecording()
-//             break
-//         }
-
-//         case Action.StopRecording: {
-//             stopRecording()
-//             break
-//         }
-//     }
-//     sendResponse();
-// });
-
 
 
 // function getTab() {
