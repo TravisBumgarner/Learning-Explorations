bda8c3be782fb007d7cdfd730c1250161a81bc74
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-11-07 15:41:14 -0600

    initial commit

---
+++ presentation-xss/codegolf.js
@@ -0,0 +1,34 @@
+let total = 5;
+
+let exmaple;
+
+let example = 'Regular'
+for (let counter = 1; counter <= 5; counter++) {
+    console.log(example, counter)
+}
+
+example = 'Minify'
+for (let i = 1; i <= 5; i++) { console.log(example, i) }
+
+example = "Code Golf Part 1 - Crafty Comparisons"
+for (let counter = 1; counter < 6; counter++) {
+    console.log(example, counter)
+}
+
+example = 'Code Golf Part 2 - Global Variables'
+for (counter = 1; counter <= 5; counter++) {
+    console.log(example, counter)
+}
+
+example = "Code Golf Part 3 - Two Things at Once"
+for (let counter = 0; ++counter <= 5;) {
+    console.log(example, counter)
+}
+
+example = "Code Golf Part 4 - No Semicolons Needed"
+for (let counter = 0; ++counter <= 5;) {
+    console.log(example, counter)
+}
+
+example = "Putting it Together"
+for(i=0;++i<6;)console.log(example,i)
---
+++ security-xss/sanitization/src/App/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+import { Body, Title } from 'sharedComponents'
+
+const App = () => {
+  return (
+    <Body>
+      <Title>Hello World!</Title>
+    </Body>
+  )
+}
+
+export default App
