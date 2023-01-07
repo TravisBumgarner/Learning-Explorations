74f248421e164eea7c3602b4a74f37211c95ea49
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-07 16:51:56 -0700

    refactored code

---
+++ technology-electron-3/electron/main.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+
+const createWindow = ({ x, y, width, height, file, preload }) => {
+    const win = new BrowserWindow({
+        width,
+        height,
+        x,
+        y,
+        webPreferences: {
+            preload,
+        },
+    })
+    ipcMain.handle('ping', () => 'pong')
+    win.loadFile(file)
+}
+
+app.whenReady().then(() => {
+    createWindow({
+        width: 500,
+        height: 500,
+        x: 0,
+        y: 0,
+        file: path.join(__dirname, '../frontend/index.html'),
+        preload: path.join(__dirname, '../frontend/preload.js')
+    })
+})
--- technology-electron-3/frontend/renderer.js
+++ technology-electron-3/frontend/renderer.js
@@ -2,7 +2,7 @@ const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${foobar.chrome()}), Node.js (v${foobar.node()}), and Electron (v${foobar.electron()})`
 
 const func = async () => {
-    const response = await window.foobar.ping()
+    const response = await foobar.ping()
     console.log(response) // prints out 'pong'
 }
 
