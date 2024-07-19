7ca36e1d28c6e6e6b66b62164a1cd4338fb6e804
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-07-19 13:58:00 -0600

    completed

--- 2024_7_14 Godot/scripts/coin.gd
+++ 2024_7_14 Godot/scripts/coin.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+
+
 func _on_body_entered(body):
 	print("enter")
 	queue_free()
+	
+	game_manager.add_point()
---
+++ 2024_7_14 Godot/scripts/gamemanager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var score = 0
+
+func add_point():
+	score += 1
+	print(score)
