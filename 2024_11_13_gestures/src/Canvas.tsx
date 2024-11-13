256b9063b3936727a2d35b411742940acdd50442
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-13 14:43:43 -0600

    done

--- 2024_11_13_gestures/src/Canvas.tsx
+++ 2024_11_13_gestures/src/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { styled } from "styled-components";
 
 function copyTouch({
@@ -14,19 +14,24 @@ function copyTouch({
 }
 
 function colorForTouch(touch: Touch) {
-	let r = touch.identifier % 16;
-	let g = Math.floor(touch.identifier / 3) % 16;
-	let b = Math.floor(touch.identifier / 7) % 16;
-	const color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
-	console.log(`color for touch with identifier ${touch.identifier} = ${color}`);
-	return color;
+    const { pageX, pageY } = touch;
+    const maxX = window.innerWidth;
+    const maxY = window.innerHeight;
+
+    const r = Math.floor((pageX / maxX) * 255);
+    const g = Math.floor((pageY / maxY) * 255);
+    const b = Math.floor(((pageX + pageY) / (maxX + maxY)) * 255);
+
+    const color = `rgb(${r},${g},${b})`;
+    console.log(`color for touch with identifier ${touch.identifier} = ${color}`);
+    return color;
 }
 
 const Canvas = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const ongoingTouchesRef = useRef<any[]>([]);
 
-	function ongoingTouchIndexById(idToFind: number) {
+    function ongoingTouchIndexById(idToFind: number) {
 		for (let i = 0; i < ongoingTouchesRef.current.length; i++) {
 			const id = ongoingTouchesRef.current[i].identifier;
 
@@ -37,63 +42,53 @@ const Canvas = () => {
 		return -1; // not found
 	}
 
-	const handleStart = (evt: TouchEvent) => {
+	const handleStart = useCallback((evt: TouchEvent) => {
 		evt.preventDefault();
-		console.log("touchstart.");
+		// console.log("touchstart.");
 		const ctx = canvasRef.current?.getContext("2d");
 		const touches = evt.changedTouches;
-
+        // console.log('touches', touches)
 		if (!ctx) return;
 
 		for (let i = 0; i < touches.length; i++) {
 			console.log(`touchstart: ${i}.`);
 			ongoingTouchesRef.current.push(copyTouch(touches[i]));
-			const color = colorForTouch(touches[i]);
-			console.log(`color of touch with id ${touches[i].identifier} = ${color}`);
-			ctx.beginPath();
-			ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false); // a circle at the start
-			ctx.fillStyle = color;
-			ctx.fill();
 		}
-	};
+	}, []);
 
-	const handleEnd = (evt: TouchEvent) => {
+	const handleEnd = useCallback((evt: TouchEvent) => {
         evt.preventDefault();
-        console.log("touchend");
+        // console.log("touchend");
         const ctx = canvasRef.current?.getContext("2d");
         if (!ctx) return
 
         const touches = evt.changedTouches;
       
         for (let i = 0; i < touches.length; i++) {
-          const color = colorForTouch(touches[i]);
+        //   const color = colorForTouch(touches[i]);
           let idx = ongoingTouchIndexById(touches[i].identifier);
       
           if (idx >= 0) {
-            ctx.lineWidth = 4;
-            ctx.fillStyle = color;
-            ctx.beginPath();
-            ctx.moveTo(ongoingTouchesRef.current[idx].pageX, ongoingTouchesRef.current[idx].pageY);
-            ctx.lineTo(touches[i].pageX, touches[i].pageY);
-            ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8); // and a square at the end
             ongoingTouchesRef.current.splice(idx, 1); // remove it; we're done
           } else {
             console.log("can't figure out which touch to end");
           }
         }
-	};
+	}, [])
 
-	const handleCancel = (e: TouchEvent) => {
+	const handleCancel = useCallback((e: TouchEvent) => {
         e.preventDefault();
-        console.log("touchcancel.");
+        // console.log("touchcancel.");
         const touches = e.changedTouches;
 
         for (let i = 0; i < touches.length; i++) {
             ongoingTouchesRef.current.splice(i, 1); // remove it; we're done
         }
-	};
+	}, [])
 
-	const handleMove = (evt: TouchEvent) => {
+	const handleMove = useCallback((evt: TouchEvent) => {
+        console.log(ongoingTouchesRef.current)
+
 		evt.preventDefault();
 		const ctx = canvasRef.current?.getContext("2d");
 		const touches = evt.changedTouches;
@@ -105,16 +100,16 @@ const Canvas = () => {
 			const idx = ongoingTouchIndexById(touches[i].identifier);
 
 			if (idx >= 0) {
-				console.log(`continuing touch ${idx}`);
+				// console.log(`continuing touch ${idx}`);
 				ctx.beginPath();
-				console.log(
-					`ctx.moveTo( ${ongoingTouchesRef.current[idx].pageX}, ${ongoingTouchesRef.current[idx].pageY} );`
-				);
+				// console.log(
+				// 	`ctx.moveTo( ${ongoingTouchesRef.current[idx].pageX}, ${ongoingTouchesRef.current[idx].pageY} );`
+				// );
 				ctx.moveTo(
 					ongoingTouchesRef.current[idx].pageX,
 					ongoingTouchesRef.current[idx].pageY
 				);
-				console.log(`ctx.lineTo( ${touches[i].pageX}, ${touches[i].pageY} );`);
+				// console.log(`ctx.lineTo( ${touches[i].pageX}, ${touches[i].pageY} );`);
 				ctx.lineTo(touches[i].pageX, touches[i].pageY);
 				ctx.lineWidth = 4;
 				ctx.strokeStyle = color;
@@ -125,7 +120,7 @@ const Canvas = () => {
 				console.log("can't figure out which touch to continue");
 			}
 		}
-	};
+	}, []);
 
 	useEffect(() => {
 		if (!canvasRef.current) return;
@@ -143,7 +138,7 @@ const Canvas = () => {
 			canvas.removeEventListener("touchcancel", handleCancel);
 			canvas.removeEventListener("touchmove", handleMove);
 		};
-	}, [canvasRef]);
+	}, [canvasRef, handleStart, handleEnd, handleCancel, handleMove]);
 
 	return <StyledCanvas ref={canvasRef} width={800} height={600}></StyledCanvas>;
 };
