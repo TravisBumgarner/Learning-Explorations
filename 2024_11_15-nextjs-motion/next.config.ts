02f4867981eb2208127b6b78efab395ab78f7418
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-15 19:18:31 -0600

    can deploy to gcp

--- 2024_11_15-nextjs-motion/next.config.ts
+++ 2024_11_15-nextjs-motion/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone' // for docker
+}
 
-export default nextConfig;
+export default nextConfig
