0acd81d04e2588c266dd01c3ce81de6479e37ff0
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-25 20:21:50 -0600

    danggg

--- 2024_11_25_framer_motion/src/Home.tsx
+++ 2024_11_25_framer_motion/src/Home.tsx
@@ -4,6 +4,11 @@ const Home = () => {
 	return (
 		<RouteWrapper>
 			<h1>Home</h1>
+			<h2>Default theming is sexy.</h2>
+			<p>
+				(With this random font I found on the internet. That's a bit too bold
+				and looks bad at small font sizes lol.)
+			</p>
 		</RouteWrapper>
 	);
 };
